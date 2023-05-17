b6d36515f5176dec53879e3682a47b788b63c8f6
air <kobychev00@bk.ru>
2023-05-18 00:33:41 +0300

    не отвечает после добавление @GetMapping Whitelabel Error Page
                                             This application has no explicit mapping for /error, so you are seeing this as a fallback.
    
                                             Thu May 18 00:30:24 MSK 2023
                                             There was an unexpected error (type=Not Found, status=404).

---
+++ src/main/java/com/java/demoexc/DemoexcApplication.java
@@ -0,0 +1,13 @@
+package com.java.demoexc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoexcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoexcApplication.class, args);
+    }
+
+}
